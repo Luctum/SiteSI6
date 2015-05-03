cf3fe462832ed63f1c2c07cf2d90b484776f6df4
Luctum <hugo.palla@gmail.com>
2015-05-04 00:36:17 +0200

    Changement nom de variable

--- controler/articles/controleur_article_clique.php
+++ controler/articles/controleur_article_clique.php
@@ -1,5 +1,5 @@
 <?php
    	require_once $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/controler/articles/controleur_article.php';
-    $ctrlAccueil = new ControleurArticle();
-    $ctrlAccueil->afficheArticle();
+    $ctrlArticle = new ControleurArticle();
+    $ctrlArticle->afficheArticle();
 ?>

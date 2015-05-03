1f990eeccdbe2f7805f9b7900adeb584bf70d6d0
Luctum <hugo.palla@gmail.com>
2015-05-03 23:58:07 +0200

    Test

--- views/contenu/vue_accueil.php
+++ views/contenu/vue_accueil.php
@@ -5,7 +5,7 @@
 				précis et vous étes perdus dans la masse d’information disponible., n’est-ce pas ?<br/>
 				Notre site Cognitio à pour but de regrouper les connaissances des utilisateurs, formulées sous forme de syntheses. <br/>
 				Chaque connaissance est vérifiée puis validée par un modérateur, ce qui fait que Cognitio est une source plus fiable qu’une grande <br/>
-				majorité de ce que l’on trouve sur le net !</p>
+				majorité de ce que l’on trouve sur le net !222</p>
 		</div>	
 	<a class="bouton_bas">ACCEDER AUX CONNAISSANCES</a>	
 </article>

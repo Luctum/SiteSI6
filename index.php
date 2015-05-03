88537044ac6847863c59ee67f2fba2ccc6efd316
Luctum <hugo.palla@gmail.com>
2015-05-04 00:22:44 +0200

    Test GIT PC Portable

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-﻿<?php
-	require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/controler/accueil/controleur_accueil_clique.php';
+<?php
+	require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/controler/accueil/controleur_accueil_clique.php'; /* Test GIT*/
 ?>

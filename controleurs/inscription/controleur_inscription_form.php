096539a6a40558f89c53f3a1885de866658021ff
Luctum <hugo.palla@gmail.com>
2015-05-15 21:13:42 +0200

    Correction d'une erreur glissée dans le commit précédent

--- controleurs/inscription/controleur_inscription_form.php
+++ controleurs/inscription/controleur_inscription_form.php
@@ -1,14 +1,14 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/SITEBOUSILLE/modeles/utilisateur.php';
 
-class formCreationArticle {
+class formInscription{
 
     private $user;
     public function __construct(){
         $this->user = new Utilisateur();
     }
 
-    public function creation($pseudo, $login, $password, $mail){
+    public function inscription($pseudo, $login, $password, $mail){
         session_start();
         $row = $this->user->setUser($pseudo, $login, $password, $mail);
         header ("Location:  /SITEBOUSILLE ");
--- controleurs/inscription/controleur_inscription_form_clique.php
+++ controleurs/inscription/controleur_inscription_form_clique.php
@@ -1,6 +1,6 @@
 <?php
     require_once $_SERVER['DOCUMENT_ROOT'].'/SITEBOUSILLE/controleurs/inscription/controleur_inscription_form.php';
 
-    $formCreationArticle = new formCreationArticle();
-    $formCreationArticle->creationArticle($_POST['pseudo'], $_POST['login'], $_POST['password'], $_POST['mail']);
+    $formInscription = new formInscription();
+    $formInscription->inscription($_POST['pseudo'], $_POST['login'], $_POST['password'], $_POST['mail']);
 ?>

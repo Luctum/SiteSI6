62e72be37eff870007574c0a496c7c4696e847fd
Luctum <hugo.palla@gmail.com>
2015-05-17 20:31:10 +0200

    Formulaires Admin wip

--- vues/contenu/administration/vue_administrationA.php
+++ vues/contenu/administration/vue_administrationA.php
@@ -4,52 +4,76 @@
             <h3>Gestion Utilisateur</h3>
             <fieldset>
                 <legend>Bannir un utilisateur :</legend>
-            <form action="" method=""></form>
+            <form action="" method="">
+                Nom : <input type="text" name="nomUtilisateur"/><br/>
+                Date de Fin : <input type="datetime"/><br/>
+                Raison : <input type="datetime"/><br/>
+                <input type="submit">
+            </form>
             </fieldset>
             <br/>
             <fieldset>
                 <legend>Supprimer un utilisateur :</legend>
-            <form action="" method=""></form>
+            <form action="" method="">
+                Nom : <input type="text" name="nomUtilisateur"></input>
+                <br/><input type="submit">
+            </form>
+
             </fieldset>
             <br/>
             <fieldset>
                 <legend>Ajouter un utilisateur à un groupe :</legend>
-            <form action="" method=""></form>
+            <form action="" method="">
+                Nom : <input type="text" name="nomUtilisateur"></input>
+                Groupe : <select name="groupe">
+
+
+                </select>
+                 <br/><input type="submit">
+            </form>
             </fieldset>
 
             <h3>Gestion Theme</h3>
             <fieldset>
                 <legend>Ajouter un theme :</legend>
-            <form action="" method=""></form>
+            <form action="" method="POST">
+                Nom : <input type="text" name="libelleTheme"></input><br/>
+                Theme Parent : <select name="themeParent">
+
+                </select>
+                <br/><input type="submit">
+            </form>
+
             </fieldset>
             <br/>
             <fieldset>
                 <legend>Supprimer un theme :</legend>
-            <form action="" method=""></form>
+            <form action="" method="">
+                <select name="libelleTheme"></select>
+                <br/><input type="submit">
+            </form>
             </fieldset>
             <br/>
             <fieldset>
                 <legend>Valider un theme :</legend>
-            <form action="" method=""></form>
-            </fieldset>
-
-            <h3>Gestion des Connaissances</h3>
-            <fieldset>
-                <legend>Valider une connaissance :</legend>
-            <form action="" method=""></form>
+            <form action="" method="">
+               <select name="libelleTheme"></select>
+                <br/><input type="submit"></form>
             </fieldset>
 
             <h3>Gestion des Groupes</h3>
             <fieldset>
                 <legend>Ajouter un groupe :</legend>
-            <form action="" method=""></form>
+            <form action="" method=""> Nom : <input type="text" name="nomGroupe"/><br/>
+                <input type="submit"></form>
             </fieldset>
             <br/>
             <fieldset>
                 <legend>Supprimer un groupe :</legend>
-            <form action="" method=""></form>
+            <form action="" method="">
+               Nom : <select name="libelleGroupe"></select>
+                <br/><input type="submit"></form>
             </fieldset>
-
 
         </div>
     </article>

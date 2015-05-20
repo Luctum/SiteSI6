903dc2149f5f64410913f1bdba6015ed714fc96c
Luctum <hugo.palla@gmail.com>
2015-05-20 17:30:03 +0200

    Securité pour l'acces à la modif

--- controleurs/articles/edition/controleur_edition_article.php
+++ controleurs/articles/edition/controleur_edition_article.php
@@ -2,24 +2,35 @@
 
 class ControleurEditionArticle {
 
-    public function afficheEditionArticle(){
+    public function afficheEditionArticle($id){
         session_start();
 
-        require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/vues/header/menu.php';   // Le test se trouve dans la vue
+        require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/vues/header/menu.php';
+        require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/modeles/article.php';
         require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/modeles/themes.php';
 
-        if(isset($_SESSION['login'])){
+        $article = new Article;
+        $ex = $article->afficheMessageConnaissances();
+        $ex = $ex->fetch();
+
+        if(isset($_SESSION['login'])){ //Empeche l'acces a la page par des invités
 
             require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/vues/header/headerC.php'; ?>
-            <script type="text/javascript" src='<?php $_SERVER['DOCUMENT_ROOT'] ?>/SITEBOUSILLE/lib/verifTitre.js'></script>
-<?php
-            require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/vues/contenu/articles/vue_article_edition.php';
+            <script type="text/javascript" src='<?php $_SERVER['DOCUMENT_ROOT'] ?>/SITEBOUSILLE/lib/verifForm.js'></script>
+
+<?php       if((isset($_SESSION['securite'])) && ($_SESSION['securite'] == 3)){ //Autorise l'acces aux administrateurs
+                require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/vues/contenu/articles/vue_article_edition.php';
+            }
+            elseif($_SESSION['pseudo'] == $ex['pseudo']){ //Autorise cependant l'acces si celui qui veux modifier est l'auteur
+                require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/vues/contenu/articles/vue_article_edition.php';
+            }
+            else{ //Renvoie sur la page d'accueil l'utilisateur modérateur/non connécté/non auteur sur la liste
+                header ("Location:  /SITEBOUSILLE/controleurs/articles/controleur_article_clique.php ");
+           }
         }
         else{
-            require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/vues/header/headerNC.php';
-            $contenu = "Vous n'etes pas connécté, vous ne pouvez pas faire ça...";
+             header ("Location:  /SITEBOUSILLE/controleurs/articles/controleur_article_clique.php ");
         }
-
 
         require $_SERVER['DOCUMENT_ROOT'].'SITEBOUSILLE/vues/gabari.php';
     }

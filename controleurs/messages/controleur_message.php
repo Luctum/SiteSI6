154f748d71ad2461a40f29c6fb9a60d84d0719c7
Luctum <hugo.palla@gmail.com>
2015-05-21 20:42:36 +0200

    Oubli du dernier commit

--- controleurs/messages/controleur_message.php
+++ controleurs/messages/controleur_message.php
@@ -7,20 +7,22 @@ class ControleurMessage {
         session_start();
         require $_SERVER['DOCUMENT_ROOT'] . 'SITEBOUSILLE/vues/header/menu.php';
         if (isset($_SESSION['login'])) {
+            require $_SERVER['DOCUMENT_ROOT'] . 'SITEBOUSILLE/modeles/messages.php';
+            $message = new Message();
+            $newMp = $message->verifMessage();
             require $_SERVER['DOCUMENT_ROOT'] . 'SITEBOUSILLE/vues/header/headerC.php';
         } else {
             require $_SERVER['DOCUMENT_ROOT'] . 'SITEBOUSILLE/vues/header/headerNC.php';
         }
-        require $_SERVER['DOCUMENT_ROOT'] . 'SITEBOUSILLE/modeles/messages.php';
-        require $_SERVER['DOCUMENT_ROOT'] . 'SITEBOUSILLE/modeles/themes.php';
-
-        $message = new Message();
+        $message2 = new Message();
+        $message2->valideMessage($_SESSION['userid']);
         $ex = $message->afficheMessage();
         $rows = $ex->fetchAll(PDO::FETCH_ASSOC);
+
+
         require $_SERVER['DOCUMENT_ROOT'] . 'SITEBOUSILLE/vues/contenu/messages/vue_message.php';
         require $_SERVER['DOCUMENT_ROOT'] . 'SITEBOUSILLE/vues/gabari.php';
     }
-
 }
 
 ?>
